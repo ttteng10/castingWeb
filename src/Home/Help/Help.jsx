620f00ff08ca631c3c1e71d352b4efadf9f38998
ttteng10 <110088222+ttteng10@users.noreply.github.com>
2025-04-02 09:49:20 +0900

    help Page

--- src/Home/Help/Help.jsx
+++ src/Home/Help/Help.jsx
@@ -1,18 +1,24 @@
+import { useState, useEffect } from "react";
 import styles from "./Help.module.css";
 
 export default function Help() {
+  const [helpState, setHelpState] = useState(true);
   return (
     <div className={styles.HelpWrapper}>
       <p className={styles.HelpWord}>
         원하는 웹툰을 선택해 주인공 배역에 어울리는 배우들을 추천해주세요!!!
       </p>
-      <p className={styles.HelpWord}>
-        원하는 웹툰을 선택해 주인공 배역에 어울리는 배우들을 추천해주세요!!!
-      </p>
-      <p className={styles.HelpWord}>
-        원하는 웹툰을 선택해 주인공 배역에 어울리는 배우들을 추천해주세요!!!
-      </p>
-      <p className={styles.HelpWord}>문의사항 : Email~~~</p>
+      <p className={styles.HelpWord}>문의사항 및 필요사항 남겨주세요!!!</p>
+      <div className={styles.noticeBoard}>
+        <div className={styles.noticeAdd}>
+          <div className={styles.AddBtn} onClick={() => setHelpState(false)}>
+            문의하기
+          </div>
+        </div>
+        <div className={styles.notices}>
+          <div className={styles.notice}></div>
+        </div>
+      </div>
     </div>
   );
 }
